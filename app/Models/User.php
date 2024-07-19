df68d8a45cdad366f8b08075db3a69f8551f0f15
BuildTools <unconfigured@null.spigotmc.org>
2024-07-19 23:01:35 +0000

    lunar installation

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Lunar\Base\Traits\LunarUser;
 
 class User extends Authenticatable
 {
+    use LunarUser;
     use HasFactory, Notifiable;
 
     /**
